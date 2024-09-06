2677410439054fa3f4c65432a479354a645193e1
Faezeh Roohbakhsh <froohbakhsh@gmail.com>
2024-09-06 10:48:53 +0330

    Add Functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
+
+'use client'
+
 import Image from "next/image";
+import { json } from "stream/consumers";
+import { getCurrentWeather } from "./services/api"
+import { useState } from "react";
+import { WeatherData } from './types/server'
+
+  interface City {
+    id: number;
+    name: string;
+    lat: string;
+    lon: string;
+  }
+
+ const cities = [
+  {id: 1, name: 'tehran', lat: "354300.12", lon:"512400.00"},
+  {id: 2, name: 'ahvaz', lat: "31.3183", lon:"48.6706"},
+  {id: 3, name: 'ardabil', lat: "38.2432", lon:"48.2976"},
+ ];
 
 export default function Home() {
+
+  const [weatherData, setWeatherData] = useState<WeatherData>()
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+
+    const { value } = e.target;
+    const selectedLocation: City = JSON.parse(value)
+    getCurrentWeather({lat: selectedLocation.lat, lon: selectedLocation.lon}).then((res) => {
+      setWeatherData(res);
+    })
+  }
+
   return (
     <>
       <h1>Weather App</h1>
+
+      <h3>Pressure: {weatherData?.main.pressure}</h3>
+      <h3>Temp: {weatherData?.main.temp}</h3>
+      <h3>Wind Speed: {weatherData?.wind.speed}</h3>
+
+      <select onChange={handleChange}>
+        {
+          cities.map(item => 
+              <option key={item.id} value={JSON.stringify(item)}>{item.name}</option>
+            )
+        }
+      </select>
     </>
   )
 }
---
+++ src/app/services/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const APIKey = "2e6adf700cd25b43c952169bd8ef9d30";
+
+const client = axios.create(
+    {
+        baseURL: "https://api.openweathermap.org/data/2.5"
+    }
+)
+
+export async function getCurrentWeather({lat, lon}: {lat: string, lon: string}){
+
+   const {data} = await client(`/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`)
+   return data
+}
